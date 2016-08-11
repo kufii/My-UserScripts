197e4b083d7db6cfae97ec0a0f1babd189134cfb
kufii <adpyke@gmail.com>
2016-08-10 21:56:20 -0400

    added Google Music Confirm Close

---
+++ google-music-confirm-close.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Google Music Confirm Close
+// @namespace    https://greasyfork.org/users/649
+// @version      1.0.8
+// @description  Confirms close when playing music on google music
+// @author       Adrien Pyke
+// @match        *://play.google.com/music/listen*
+// @grant        unsafeWindow
+// @noframes
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	unsafeWindow.onbeforeunload = function (e) {
+		var button = document.querySelector('paper-icon-button[data-id="play-pause"]');
+		if (button.classList.contains('playing'))
+			return 'Are you sure you want to close Google Music?';
+	};
+})();
