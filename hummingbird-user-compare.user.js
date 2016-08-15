06bb3d49da647ac1052eaa04d127330f2889794d
Adrien Pyke <adpyke@gmail.com>
2016-08-14 21:44:00 -0400

    Hummingbirg User Compare Rewrite

--- hummingbird-user-compare.user.js
+++ hummingbird-user-compare.user.js
@@ -1,13 +1,10 @@
 // ==UserScript==
 // @name         Hummingbird User Compare
 // @namespace    https://greasyfork.org/users/649
-// @version      3.0.16
+// @version      3.1
 // @description  Adds a button that compares the anime list of a hummingbird user against yours
 // @author       kufii, fuzetsu
-// @match        http://hummingbird.me/*
-// @match        http://forums.hummingbird.me/*
-// @match        https://hummingbird.me/*
-// @match        https://forums.hummingbird.me/*
+// @match        *://hummingbird.me/*
 // @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=122976
 // @grant        none
 // @noframes
@@ -17,9 +14,9 @@
 	'use strict';
 
 	var SCRIPT_NAME = 'Hummingbird User Compare';
-	var BTN_ID = 'us-compare-button';
-	var COMPAT_ID = 'us-compat-table';
-	var USERNAME_SELECTOR = 'h2.username,h1.username';
+	var BTN_ID = 'huc-compare-button';
+	var COMPAT_ID = 'huc-compat-table';
+	var USERNAME_SELECTOR = 'h2.username';
 	var CACHE = {};
 
 	var Util = {
@@ -47,55 +44,6 @@
 
 			return texts.join("");
 		},
-		arrToTable: function(arr, opts) {
-			// takes an object and converts to an html compliant list of attributes
-			var makeAttrs = function(obj) {
-				var str = '';
-				var key;
-				for(key in obj) {
-					if(obj.hasOwnProperty(key)) {
-						str += key + '="' + obj[key] + '" ';
-					}
-				}
-				return str;
-			};
-
-			// creates an object of the specified type
-			// either takes a config object with attributes and a value, or just the value directly
-			var createElement = function(type, data) {
-				if(typeof data === 'object') {
-					return '<' + type +' ' + makeAttrs(data.attrs || {}) + '>' + data.value + '</' + type + '>';
-				}
-				return '<' + type + '>' + data + '</' + type + '>';
-			};
-
-			opts = opts || {};
-
-			var headers = '';
-
-			// if the table is going to have a head use the first row
-			if(opts.hasHead) {
-				headers = arr[0].map(function(header) {
-					return createElement('th', header);
-				}).join('');
-				headers = '<thead>' + headers + '</thead>';
-				arr = arr.slice(1);
-			}
-
-			// create the body of the array
-			var rows = arr.map(function(row) {
-				return '<tr>' + row.map(function(cell) {
-					return createElement('td', cell);
-				}).join('') + '</tr>';
-			}).join('');
-
-			// create and return the actual table all put together
-			return createElement('table', {
-				attrs: opts.attrs,
-				value: (headers + '<tbody>' + rows + '</tbody>')
-			});
-
-		},
 		getJSON: function(url, load, error) {
 			var xhr = new XMLHttpRequest();
 			xhr.open('get', url);
@@ -105,9 +53,6 @@
 			};
 			xhr.onerror = error;
 			xhr.send();
-		},
-		titleCase: function(str) {
-			return str.charAt(0).toUpperCase() + str.slice(1);
 		}
 	};
 
@@ -155,130 +100,110 @@
 			// get manga compat
 			Util.getJSON('https://hbird-cmp-node.herokuapp.com/compatibility/manga?user1=' + you + '&user2=' + them, process);
 		},
-		makeCompatTable: function(compats, style) {
-			var table = compats.map(function(compat) {
-				var message = compat.phrase;
-				if (compat.value) {
-					message += ' (' + compat.percent+  ')';
-				}
-				return [
-					{value: Util.titleCase(compat.type) + ' Compatibility:', attrs: {style: 'text-align: right; padding-right: 5px;'}},
-					{value: message, attrs: {style: 'font-weight:bold'}}
-				];
+		getTable: function() {
+			var table = document.createElement('table');
+			table.id = COMPAT_ID;
+			table.style.position = 'absolute';
+			table.style.bottom = '-25px';
+			table.style.right = '0';
+
+			var types = [{
+				type: 'Anime:',
+				id: 'huc-anime'
+			}, {
+				type: 'Manga:',
+				id: 'huc-manga'
+			}];
+
+			types.forEach(function(type) {
+				var tr = document.createElement('tr');
+
+				var tdType = document.createElement('td');
+				tdType.textContent = type.type;
+				tdType.style.textAlign = 'right';
+				tdType.style.paddingRight = '5px';
+				tr.appendChild(tdType);
+
+				var tdValue = document.createElement('td');
+				tdValue.id = type.id;
+				tdValue.style.fontWeight = 'bold';
+				tr.appendChild(tdValue);
+
+				table.appendChild(tr);
 			});
-			return Util.arrToTable(table, {
-				hasHead: false,
-				attrs: {
-					style: 'position: absolute; bottom: -25px; right: 0;' + (style || ''),
-					id: COMPAT_ID
-				}
-			});
+
+			return table;
 		}
 	};
 
-	var lastUser;
-
 	Util.log('Started, waiting for user page...');
 
-	waitForUrl(/https:\/\/(forums\.)?hummingbird\.me\/users\/.+/, function() {
+	waitForUrl(/https?:\/\/hummingbird\.me\/users\/.+/, function() {
 		// not signed in
-		if(Util.q('.signup-cta')) return;
-
-		var compatTable = Util.q('#' + COMPAT_ID);
-		var btn = Util.q('#' + BTN_ID);
-
-		// cleanup area
-		if(lastUser && location.href.indexOf('/users/' + lastUser) === -1) {
-			if(btn) btn.remove();
-			if(compatTable) compatTable.remove();
+		if(Util.q('.signup-cta')) {
+			Util.log('User not logged in.');
+			return;
 		}
 
 		Util.log('Found user page, waiting for button area...');
-		waitForElems('.user-cover-options .follow-button:not(.' + BTN_ID + ')', function(btnFollow) {
-
-			// exit early if you're on your own profile
+		waitForElems('.user-cover-options .follow-button:not(#' + BTN_ID + ')', function(btnFollow) {
 			var compare = hb.getCompareUrl();
-			if(location.href.indexOf('/users/' + compare.you) !== -1) {
-				lastUser = compare.you;
+			var btn = Util.q('#' + BTN_ID);
+			var compat = Util.q('#' + COMPAT_ID);
+			// exit early if you're on your own profile
+			if (compare.you === compare.them) {
+				Util.log('On user\'s own profile.');
+				// cleanup
+				if (btn) {
+					btn.remove();
+					btn = null;
+				}
+				if (compat) {
+					compat.remove();
+					compat = null;
+				}
 				return;
 			}
 
+			Util.log('Found button area. Adding button and compat table...');
+
 			var container = btnFollow.parentNode;
 
-			// try getting forum button and fix it if we find it
-			var forumButton = Util.q('.account-info .inline-list a');
-			if(forumButton) {
-				forumButton.title = '';
-				forumButton.target = '_blank';
-				forumButton.href = compare.url;
-			}
-			btn = forumButton || btn;
-
-			var forumButtonStyle = 'bottom: 38px; width: 350px;';
-
-			var addLoadingTable = function() {
-				var styleOverride = '';
-				if(forumButton) {
-					styleOverride = forumButtonStyle;
-				}
-				container.innerHTML += hb.makeCompatTable([
-					{ type: 'Anime', phrase: 'Loading...' },
-					{ type: 'Manga', phrase: 'Loading...' }
-				], styleOverride);
-			};
-
-			var compatibilityCallback = function(anime, manga) {
-				var styleOverride = '';
-				if(forumButton) {
-					styleOverride = forumButtonStyle;
-				}
-				var compatTable = Util.q('#' + COMPAT_ID);
-				if (compatTable) compatTable.remove();
-				console.log(btnFollow, container);
-				container.innerHTML += hb.makeCompatTable([anime, manga], styleOverride);
-			};
-
-			Util.log('Found button area, loading button and compat rating...');
-
-			if(!btn) {
+			if (btn) {
+				btn.href = compare.url;
+			} else {
 				btn = document.createElement('a');
 				btn.className = btnFollow.className;
 				btn.id = BTN_ID;
 				btn.textContent = 'Compare';
 				btn.target = '_blank';
 				btn.setAttribute('style', 'right: ' + (btnFollow.clientWidth + 10) + 'px; background: rgb(236, 134, 97); color: white;');
-			}
-			if(compare.them === lastUser) {
-				// if the current user is the same as last but the user url changed then wait for the username to update
-				if(location.href.indexOf("/users/" + lastUser) === -1) {
-					if (compatTable) {
-						compatTable.remove();
-					}
-					var handler = function(e) {
-						this.removeEventListener('DOMSubtreeModified', handler);
-						compare = hb.getCompareUrl();
-						btn.href = compare.url;
-						if(!forumButton) {
-							container.appendChild(btn);
-						}
-						addLoadingTable();
-						hb.getCompatibility(compatibilityCallback);
-					};
-					Util.q(USERNAME_SELECTOR).addEventListener('DOMSubtreeModified', handler);
-				}
-			} else {
-				if (compatTable) {
-					compatTable.remove();
-				}
 				btn.href = compare.url;
-				if(!forumButton) {
-					container.appendChild(btn);
-				}
-				addLoadingTable();
-				hb.getCompatibility(compatibilityCallback);
+				container.appendChild(btn);
 			}
-			lastUser = compare.them;
 
+			if (!compat) {
+				compat = hb.getTable();
+				container.appendChild(compat);
+			}
+			var animeNode = Util.q('#huc-anime', compat);
+			var mangaNode = Util.q('#huc-manga', compat);
+
+			animeNode.textContent = mangaNode.textContent = 'Loading...';
+
+			Util.log('Getting compatibility...');
+			hb.getCompatibility(function(anime, manga) {
+				var animeMsg = anime.phrase;
+				if (anime.value) {
+					animeMsg += ' (' + anime.percent + ')';
+				}
+				var mangaMsg = manga.phrase;
+				if (manga.value) {
+					mangaMsg += ' (' + manga.percent + ')';
+				}
+				animeNode.textContent = animeMsg;
+				mangaNode.textContent = mangaMsg;
+			});
 		}, true);
 	});
 })();
