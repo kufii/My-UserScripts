357baefaaf47c7efbb21c49a9a1ec3f7aa0e08a1
Adrien Pyke <adpyke@gmail.com>
2016-08-18 16:15:53 -0400

    Create userstyles-auto-keep-me-logged-in.user.js

---
+++ userstyles-auto-keep-me-logged-in.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         userstyles.org - auto select keep me logged in
+// @namespace    https://greasyfork.org/users/649
+// @version      1.0
+// @description  Auto checks keep me logged in on userstyles.org
+// @author       Adrien Pyke
+// @match        *://userstyles.org/login*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	document.querySelector('#remember-openid').checked = true;
+})();
