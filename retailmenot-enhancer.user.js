6a91281803c3fbc6b1987c09b6ad953b78ca9023
Adrien Pyke <adpyke@gmail.com>
2016-09-15 11:42:46 -0400

    added international support

--- retailmenot-enhancer.user.js
+++ retailmenot-enhancer.user.js
@@ -1,11 +1,15 @@
 // ==UserScript==
 // @name         RetailMeNot Enhancer
 // @namespace    https://greasyfork.org/users/649
-// @version      2.0.4
+// @version      3.0
 // @description  Auto shows coupons and stops pop-unders on RetailMeNot
 // @author       Adrien Pyke
 // @match        *://www.retailmenot.com/*
 // @match        *://www.retailmenot.ca/*
+// @match        *://www.retailmenot.de/*
+// @match        *://www.retailmenot.es/*
+// @match        *://www.retailmenot.it/*
+// @match        *://www.retailmenot.pl/*
 // @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=147465
 // @grant        GM_openInTab
 // ==/UserScript==
@@ -72,57 +76,23 @@
 		},
 		changeUrl: function(url) {
 			window.history.replaceState({ path: url }, '', url);
+		},
+		createCookie: function(name, value, days) {
+			var expires;
+			if (days) {
+				var date = new Date();
+				date.setTime(date.getTime()+(days*24*60*60*1000));
+				expires = "; expires="+date.toGMTString();
+			}
+			else expires = "";
+			document.cookie = name+"="+value+expires+"; path=/";
 		}
 	};
 
-	if (window.location.href.match(/^https?:\/\/www.retailmenot\.ca/i)) { // CANADA
-		// Show Coupons
-		Util.qq('.crux > .cover').forEach(function(cover) {
-			cover.remove();
-		});
-
-		// Disable Pop Unders
-		waitForElems({
-			sel: '.offer, .stage .coupon',
-			onmatch: function(offer) {
-				var clickHandler = function(e) {
-					e.preventDefault();
-					e.stopImmediatePropagation();
-					var href = window.location.protocol + "//" + window.location.host + window.location.pathname + '?c=' + offer.dataset.offerid;
-					if (e.button === 1) {
-						GM_openInTab(href, true);
-					} else {
-						window.location.replace(href);
-					}
-					return false;
-				};
-
-				if (!offer.parentNode.classList.contains('stage')) {
-					waitForElems({
-						context: offer,
-						sel: 'a.offer-title',
-						stop: true,
-						onmatch: function(title) {
-							title.href = href;
-							title.onclick = clickHandler;
-						}
-					});
-				}
-
-				Util.qq('.action-button, .crux, .caterpillar-title, .caterpillar-code', offer).forEach(function(elem){
-					elem.onclick = clickHandler;
-				});
-			}
-		});
-
-		// disable pop unders on the exclusive tags
-		Util.qq('.exclusive_icon').forEach(function(tag) {
-			tag.onclick = function(e) {
-				e.preventDefault();
-				e.stopImmediatePropagation();
-			};
-		});
-	} else { // US
+	// remove force reload param
+	Util.changeUrl(Util.removeQueryParameter('r'));
+	if (window.location.href.match(/^https?:\/\/www\.retailmenot\.com/i)) { // US
+		Util.log('Enhancing US site');
 		// Show Coupons
 		document.body.classList.add('ctc');
 
@@ -169,9 +139,89 @@
 				}
 			}
 		});
+	} else if (window.location.href.match(/^https?:\/\/www\.retailmenot\.ca/i)) { // CANADA
+		Util.log('Enhancing Canadian site');
+		// Show Coupons
+		Util.qq('.crux > .cover').forEach(function(cover) {
+			cover.remove();
+		});
+
+		// Disable Pop Unders
+		waitForElems({
+			sel: '.offer, .stage .coupon',
+			onmatch: function(offer) {
+				var clickHandler = function(e) {
+					e.preventDefault();
+					e.stopImmediatePropagation();
+					var href = window.location.protocol + "//" + window.location.host + window.location.pathname + '?c=' + offer.dataset.offerid;
+					if (e.button === 1) {
+						GM_openInTab(href, true);
+					} else {
+						window.location.replace(href);
+					}
+					return false;
+				};
+
+				if (!offer.parentNode.classList.contains('stage')) {
+					waitForElems({
+						context: offer,
+						sel: 'a.offer-title',
+						stop: true,
+						onmatch: function(title) {
+							title.href = href;
+							title.onclick = clickHandler;
+						}
+					});
+				}
+
+				Util.qq('.action-button, .crux, .caterpillar-title, .caterpillar-code', offer).forEach(function(elem){
+					elem.onclick = clickHandler;
+				});
+			}
+		});
+
+		// disable pop unders on the exclusive tags
+		Util.qq('.exclusive_icon').forEach(function(tag) {
+			tag.onclick = function(e) {
+				e.preventDefault();
+				e.stopImmediatePropagation();
+			};
+		});
+	} else { // GERMANY, SPAIN, ITALY, POLAND
+		Util.log('Enhancing international site');
+		// Remove hash after modal comes up
+		if (window.location.href.indexOf('#') !== -1) {
+			waitForElems({
+				sel: '#modal-coupon',
+				stop: true,
+				onmatch: function() {
+					Util.changeUrl(window.location.href.split('#')[0]);
+				}
+			});
+		}
+		// disable pop unders
+		waitForElems({
+			sel: '.coupon',
+			onmatch: function(coupon) {
+				var clickHandler = function(e) {
+					e.preventDefault();
+					e.stopImmediatePropagation();
+					Util.createCookie('click_' + coupon.dataset.suffix, true);
+					if (e.button === 1) {
+						GM_openInTab(window.location.protocol + "//" + window.location.host + window.location.pathname + '#' + coupon.dataset.suffix, true);
+					} else {
+						window.location.replace(window.location.protocol + "//" + window.location.host + window.location.pathname + '?r=1#' + coupon.dataset.suffix);
+					}
+					return false;
+				};
+				Util.qq('.outclickable', coupon).forEach(function(elem) {
+					elem.onclick = clickHandler;
+				});
+			}
+		});
 	}
 	// human checks
-	var regex = /^https?:\/\/www\.retailmenot\.(?:com|ca)\/humanCheck\.php/i;
+	var regex = /^https?:\/\/www\.retailmenot\.[^\/]+\/humanCheck\.php/i;
 	Util.qq('a').filter(function(link) {
 		return link.href.match(regex);
 	}).forEach(function(link) {
