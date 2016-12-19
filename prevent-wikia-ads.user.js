f456ce5fc5a5d6e4cb110d4d43ecdbbf122f9ab1
Adrien Pyke <adpyke@gmail.com>
2016-12-19 11:07:51 -0500

    removed unnecessary middle click handling

--- prevent-wikia-ads.user.js
+++ prevent-wikia-ads.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Prevent Wikia Ads
 // @namespace    https://greasyfork.org/users/649
-// @version      1.0
+// @version      1.0.1
 // @description  Prevent the ads that pop up when clicking a link to an external page on Wikias
 // @author       Adrien Pyke
 // @match        *://*.wikia.com/*
 // @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=122976
-// @grant        GM_openInTab
+// @grant        none
 // ==/UserScript==
 
 (function() {
@@ -16,9 +16,7 @@
 		link.onclick = function(e) {
 			e.preventDefault();
 			e.stopImmediatePropagation();
-			if (e.button === 1) {
-				GM_openInTab(link.href, true);
-			} else {
+			if (e.button === 0) {
 				location.href = link.href;
 			}
 			return false;
