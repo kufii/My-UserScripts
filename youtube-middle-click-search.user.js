3f06178a76c4637dcf75d4e06ecaf0cdbb3772f9
Adrien Pyke <adpyke@gmail.com>
2016-12-19 13:53:46 -0500

    removed custom event, use auxclick

--- youtube-middle-click-search.user.js
+++ youtube-middle-click-search.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         Youtube Middle Click Search
 // @namespace    https://greasyfork.org/users/649
-// @version      1.4.9
+// @version      1.4.10
 // @description  Middle clicking the search on youtube opens the results in a new tab
 // @author       Adrien Pyke
 // @match        *://www.youtube.com/*
 // @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=122976
-// @require      https://greasyfork.org/scripts/25833-middle-click-event/code/Middle%20Click%20Event.js?version=164257
 // @grant        GM_openInTab
 // ==/UserScript==
 
@@ -30,17 +29,6 @@
 		// insert new button and hide old (as opposed to remove, removing was conflicting with another script I use)
 		oldButton.parentNode.insertBefore(button, oldButton.nextSibling);
 		oldButton.style.display = 'none';
-		// open results function
-		var openResults  = function(newTab) {
-			if (input.value.trim() === '' || input.value.trim() === initSearch && !newTab) return;
-			var url = location.origin + '/results?search_query=' + encodeURIComponent(input.value);
-			if (newTab) {
-				console.log('opening');
-				GM_openInTab(url, true);
-			} else {
-				window.location.href = url;
-			}
-		};
 		// bind events
 		button.onmousedown = function(e) {
 			if (e.button === 1) {
@@ -50,45 +38,40 @@
 		button.onclick = function(e) {
 			e.preventDefault();
 			e.stopImmediatePropagation();
-			if(e.button === 0) {
-				openResults();
+			if (input.value.trim() === '' || input.value.trim() === initSearch && e.button !== 1) return false;
+			var url = location.origin + '/results?search_query=' + encodeURIComponent(input.value);
+			if (e.button === 1) {
+				console.log('opening');
+				GM_openInTab(url, true);
+			} else if(e.button === 0) {
+				window.location.href = url;
 			}
 			return false;
 		};
-		button.addEventListener('middleclick', function(e) {
-			openResults(true);
-		});
+		button.onauxclick = button.onclick;
 	};
 
 	var processResults = function() {
 		var elements = document.querySelectorAll('.gssb_e .gsq_a');
 		[].forEach.call(elements, function(element) {
 			if (element) {
-				var openResults = function(newTab) {
-					var url = location.origin + '/results?search_query=' + encodeURIComponent(element.querySelector('span').textContent);
-					if (newTab) {
-						console.log('opening');
-						GM_openInTab(url, true);
-					} else {
-						window.location.href = url;
-					}
-				};
 				element.onmousedown = function(e) {
 					if (e.button === 1) {
 						e.preventDefault();
 					}
 				};
 				element.onclick = function(e) {
-					e.preventDefault();
-					if(e.button === 0) {
-						openResults();
+					var url = location.origin + '/results?search_query=' + encodeURIComponent(element.querySelector('span').textContent);
+					if (e.button === 1) {
+						console.log('opening');
+						GM_openInTab(url, true);
+					} else if(e.button === 0) {
+						window.location.href = url;
 					}
+					e.preventDefault();
 					return false;
 				};
-				element.addEventListener('middleclick', function(e) {
-					e.stopImmediatePropagation();
-					openResults(true);
-				});
+				element.onauxclick = element.onclick;
 			}
 		});
 	};
