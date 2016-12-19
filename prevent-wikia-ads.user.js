cd5391602394f555e1ffa94fd2eac196aa0d5336
Adrien Pyke <adpyke@gmail.com>
2016-12-19 16:06:29 -0500

    oops

--- prevent-wikia-ads.user.js
+++ prevent-wikia-ads.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Prevent Wikia Ads
 // @namespace    https://greasyfork.org/users/649
-// @version      1.0.2
+// @version      1.0.3
 // @description  Prevent the ads that pop up when clicking a link to an external page on Wikias
 // @author       Adrien Pyke
 // @match        *://*.wikia.com/*
 // @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=122976
-// @grant        none
+// @grant        GM_openInTab
 // ==/UserScript==
 
 (function() {
