42c40305fe256491b0d5f66e49456eb69830e47e
Adrien Pyke <adpyke@gmail.com>
2016-12-19 10:28:15 -0500

    proper event propagation

--- Libraries/middle-click-event.js
+++ Libraries/middle-click-event.js
@@ -9,7 +9,8 @@
 	});
 	document.addEventListener('mouseup', function(e) {
 		if (e.button === 1 && e.target === lastTarget) {
-			var ev = new Event('middleclick');
+			var ev = document.createEvent("Events");
+			ev.initEvent('middleclick', true, true);
 			e.target.dispatchEvent(ev);
 		}
 	});
