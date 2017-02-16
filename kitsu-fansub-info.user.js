177cedcb3f2c72b6254d8cf979eb19962e97034c
synthtech <synthtech0@gmail.com>
2017-02-15 22:53:04 -0500

    Replace FontAwesome with Evil Icons

--- kitsu-fansub-info.user.js
+++ kitsu-fansub-info.user.js
@@ -8,12 +8,41 @@
 // @match        *://myanimelist.net/anime/*
 // @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=147465
 // @grant        GM_xmlhttpRequest
-// @grant        GM_addStyle
 // @grant        GM_registerMenuCommand
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_openInTab
 // ==/UserScript==
+
+/*
+
+Evil Icons
+https://github.com/evil-icons/evil-icons
+
+Copyright (c) 2014 Alexander Madyankin <alexander@madyankin.name>, Roman Shamin
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
 
 (function() {
 	'use strict';
@@ -23,7 +52,13 @@
 	var REGEX = /^https?:\/\/kitsu\.io\/anime\/([^\/]+)\/?(?:\?.*)?$/;
 	var SECTION_ID = 'kitsu-fansubs';
 
-	GM_addStyle('@import url(https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css);.fa.fa-thumbs-o-up,.fa.fa-thumbs-o-down{color:#464646;}.fa.fa-thumbs-up,.fa.fa-thumbs-o-up:hover{color:#16A085;}.fa.fa-thumbs-down,.fa.fa-thumbs-o-down:hover{color:#DB2409;}.fa.fa-smile-o{color:#16A085;}.fa.fa-frown-o{color:#DB2409;}');
+	var Icon = {
+		extLink: '<path d="M38.288 10.297l1.414 1.415-14.99 14.99-1.414-1.414z"/><path d="M40 20h-2v-8h-8v-2h10z"/><path d="M35 38H15c-1.7 0-3-1.3-3-3V15c0-1.7 1.3-3 3-3h11v2H15c-.6 0-1 .4-1 1v20c0 .6.4 1 1 1h20c.6 0 1-.4 1-1V24h2v11c0 1.7-1.3 3-3 3z"/>',
+		link: '<path d="M24 30.2c0 .2.1.5.1.8 0 1.4-.5 2.6-1.5 3.6l-2 2c-1 1-2.2 1.5-3.6 1.5-2.8 0-5.1-2.3-5.1-5.1 0-1.4.5-2.6 1.5-3.6l2-2c1-1 2.2-1.5 3.6-1.5.3 0 .5 0 .8.1l1.5-1.5c-.7-.3-1.5-.4-2.3-.4-1.9 0-3.6.7-4.9 2l-2 2c-1.3 1.3-2 3-2 4.9 0 3.8 3.1 6.9 6.9 6.9 1.9 0 3.6-.7 4.9-2l2-2c1.3-1.3 2-3 2-4.9 0-.8-.1-1.6-.4-2.3L24 30.2z"/><path d="M33 10.1c-1.9 0-3.6.7-4.9 2l-2 2c-1.3 1.3-2 3-2 4.9 0 .8.1 1.6.4 2.3l1.5-1.5c0-.2-.1-.5-.1-.8 0-1.4.5-2.6 1.5-3.6l2-2c1-1 2.2-1.5 3.6-1.5 2.8 0 5.1 2.3 5.1 5.1 0 1.4-.5 2.6-1.5 3.6l-2 2c-1 1-2.2 1.5-3.6 1.5-.3 0-.5 0-.8-.1l-1.5 1.5c.7.3 1.5.4 2.3.4 1.9 0 3.6-.7 4.9-2l2-2c1.3-1.3 2-3 2-4.9 0-3.8-3.1-6.9-6.9-6.9z"/><path d="M20 31c-.3 0-.5-.1-.7-.3-.4-.4-.4-1 0-1.4l10-10c.4-.4 1-.4 1.4 0s.4 1 0 1.4l-10 10c-.2.2-.4.3-.7.3z"/>',
+		minus: '<path d="M25 42c-9.4 0-17-7.6-17-17S15.6 8 25 8s17 7.6 17 17-7.6 17-17 17zm0-32c-8.3 0-15 6.7-15 15s6.7 15 15 15 15-6.7 15-15-6.7-15-15-15z"/><path d="M16 24h18v2H16z"/>',
+		plus: '<path d="M25 42c-9.4 0-17-7.6-17-17S15.6 8 25 8s17 7.6 17 17-7.6 17-17 17zm0-32c-8.3 0-15 6.7-15 15s6.7 15 15 15 15-6.7 15-15-6.7-15-15-15z"/><path d="M16 24h18v2H16z"/><path d="M24 16h2v18h-2z"/>',
+		thumbsUp: '<path d="M40 23.2c0-2.1-1.7-3.2-4-3.2h-6.7c.5-1.8.7-3.5.7-5 0-5.8-1.6-7-3-7-.9 0-1.6.1-2.5.6-.3.2-.4.4-.5.7l-1 5.4c-1.1 2.8-3.8 5.3-6 7V36c.8 0 1.6.4 2.6.9 1.1.5 2.2 1.1 3.4 1.1h9.5c2 0 3.5-1.6 3.5-3 0-.3 0-.5-.1-.7 1.2-.5 2.1-1.5 2.1-2.8 0-.6-.1-1.1-.3-1.6.8-.5 1.5-1.4 1.5-2.4 0-.6-.3-1.2-.6-1.7.8-.6 1.4-1.6 1.4-2.6zm-2.1 0c0 1.3-1.3 1.4-1.5 2-.2.7.8.9.8 2.1 0 1.2-1.5 1.2-1.7 1.9-.2.8.5 1 .5 2.2v.2c-.2 1-1.7 1.1-2 1.5-.3.5 0 .7 0 1.8 0 .6-.7 1-1.5 1H23c-.8 0-1.6-.4-2.6-.9-.8-.4-1.6-.8-2.4-1V23.5c2.5-1.9 5.7-4.7 6.9-8.2v-.2l.9-5c.4-.1.7-.1 1.2-.1.2 0 1 1.2 1 5 0 1.5-.3 3.1-.8 5H27c-.6 0-1 .4-1 1s.4 1 1 1h9c1 0 1.9.5 1.9 1.2z"/><path d="M16 38h-6c-1.1 0-2-.9-2-2V22c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2v14c0 1.1-.9 2-2 2zm-6-16v14h6V22h-6z"/>'
+	};
 
 	var Util = {
 		log: function() {
@@ -79,10 +114,14 @@
 			node.setAttribute('target', '_blank');
 			node.setAttribute('rel', 'noopener noreferrer');
 		},
-		icon: function(name) {
-			var icon = document.createElement('i');
-			icon.classList.add('fa', 'fa-' + name);
-			return icon;
+		icon: function(name, color) {
+			var newIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+			newIcon.innerHTML = Icon[name];
+			newIcon.setAttribute('viewBox', '0 0 50 50');
+			newIcon.setAttribute('width', '20');
+			newIcon.setAttribute('height', '20');
+			if (color) newIcon.setAttribute('fill', color);
+			return newIcon;
 		},
 		createModal: function(title, bodyDiv) {
 			var div = document.createElement('div');
@@ -454,8 +493,18 @@
 				voteDown.href = '#';
 				voteUp.dataset.value = 1;
 				voteDown.dataset.value = 2;
-				voteUp.appendChild(Util.icon(fansub.value === 1 ? 'thumbs-up' : 'thumbs-o-up'));
-				voteDown.appendChild(Util.icon(fansub.value === 2 ? 'thumbs-down' : 'thumbs-o-down'));
+
+				if (fansub.value === 1) {
+					voteUp.appendChild(Util.icon('thumbsUp', '#16A085'));
+					voteDown.appendChild(Util.icon('thumbsUp')).setAttribute('transform', 'scale(1,-1)');
+				} else if (fansub.value === 2) {
+					voteUp.appendChild(Util.icon('thumbsUp'));
+					voteDown.appendChild(Util.icon('thumbsUp','#DB2409')).setAttribute('transform', 'scale(1,-1)');
+				} else {
+					// #464646
+					voteUp.appendChild(Util.icon('thumbsUp'));
+					voteDown.appendChild(Util.icon('thumbsUp')).setAttribute('transform', 'scale(1,-1)');
+				}
 
 				var voteHandler = function(e) {
 					e.preventDefault();
@@ -470,8 +519,18 @@
 					App.vote(fansub.malid, fansub.id, value);
 					fansub.value = value;
 					voteUp.innerHTML = voteDown.innerHTML = '';
-					voteUp.appendChild(Util.icon(fansub.value === 1 ? 'thumbs-up' : 'thumbs-o-up'));
-					voteDown.appendChild(Util.icon(fansub.value === 2 ? 'thumbs-down' : 'thumbs-o-down'));
+
+					if (fansub.value === 1) {
+						voteUp.appendChild(Util.icon('thumbsUp', '#16A085'));
+						voteDown.appendChild(Util.icon('thumbsUp')).setAttribute('transform', 'scale(1,-1)');
+					} else if (fansub.value === 2) {
+						voteUp.appendChild(Util.icon('thumbsUp'));
+						voteDown.appendChild(Util.icon('thumbsUp','#DB2409')).setAttribute('transform', 'scale(1,-1)');
+					} else {
+						// #464646
+						voteUp.appendChild(Util.icon('thumbsUp'));
+						voteDown.appendChild(Util.icon('thumbsUp')).setAttribute('transform', 'scale(1,-1)');
+					}
 				};
 				voteUp.onclick = voteHandler;
 				voteDown.onclick = voteHandler;
@@ -500,7 +559,7 @@
 							var smileContainer = document.createElement('div');
 							smileContainer.classList.add('review-avatar');
 							div.appendChild(smileContainer);
-							smileContainer.appendChild(comment.approves ? Util.icon('smile-o') : Util.icon('frown-o')).setAttribute('style', 'font-size: 25px;');
+							smileContainer.appendChild(comment.approves ? Util.icon('plus', '#16A085') : Util.icon('minus', '#DB2409'));
 
 							var commentContainer = document.createElement('div');
 							commentContainer.classList.add('comment-body');
@@ -556,9 +615,8 @@
 							var malLink = document.createElement('a');
 							malLink.href = response.url;
 							Util.setNewTab(malLink);
-							malLink.setAttribute('style', 'margin-left: 5px;');
 							extLink.appendChild(malLink);
-							malLink.appendChild(Util.icon('external-link'));
+							malLink.appendChild(Util.icon('extLink')).setAttribute('style', 'vertical-align: sub;');
 
 							var list = Util.q('.media-list', section);
 
