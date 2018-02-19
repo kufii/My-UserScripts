9c98749907efe6fd515e63cf114a20c7fee1ed40
Adrien Pyke <adpyke@gmail.com>
2018-02-19 14:37:36 -0500

    Update greasyfork-enable-syntax-highlighting.user.js

--- greasyfork-enable-syntax-highlighting.user.js
+++ greasyfork-enable-syntax-highlighting.user.js
@@ -1,19 +1,23 @@
 // ==UserScript==
 // @name         Greasy Fork - Auto Enable Syntax-Highlighting Source Editor
 // @namespace    https://greasyfork.org/users/649
-// @version      1.0.1
+// @version      1.0.2
 // @description  Auto enables greasy fork's syntax-highlighting source editor
 // @author       Adrien Pyke
 // @match        *://greasyfork.org/*/script_versions/new*
 // @match        *://greasyfork.org/*/scripts/*/versions/new*
-// @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=122976
+// @require      https://cdn.rawgit.com/fuzetsu/userscripts/477063e939b9658b64d2f91878da20a7f831d98b/wait-for-elements/wait-for-elements.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
 	'use strict';
 
-	waitForElems('#enable-source-editor-code', function(checkbox) {
-		checkbox.click();
-	}, true);
+	waitForElems({
+		sel: '#enable-source-editor-code',
+		stop: true,
+		onmatch: function(checkbox) {
+			checkbox.click();
+		}
+	});
 })();
