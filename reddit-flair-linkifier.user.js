7f07e612ada23c37838bacd1a0e41050f7a2921a
Adrien Pyke <adpyke@gmail.com>
2018-02-19 14:49:05 -0500

    Update reddit-flair-linkifier.user.js

--- reddit-flair-linkifier.user.js
+++ reddit-flair-linkifier.user.js
@@ -1,24 +1,27 @@
 // ==UserScript==
 // @name         Reddit Flair Linkifier
 // @namespace    https://greasyfork.org/users/649
-// @version      1.1.4
+// @version      1.1.5
 // @description  Turns the text in various subreddits' flair into links
 // @author       Adrien Pyke
 // @match        *://*.reddit.com/*
-// @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=122976
+// @require      https://cdn.rawgit.com/fuzetsu/userscripts/477063e939b9658b64d2f91878da20a7f831d98b/wait-for-elements/wait-for-elements.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
 	'use strict';
 
-	waitForElems('span.flair', function(flair) {
-		flair.innerHTML = flair.textContent.split(' ').map(function(segment) {
-			if (segment.match(/^https?:\/\//)) {
-				return '<a href="' + segment + '" target="_blank" rel="noopener noreferrer">' + segment + '</a>';
-			} else {
-				return segment;
-			}
-		}).join(' ');
+	waitForElems({
+		sel: 'span.flair',
+		onmatch: function(flair) {
+			flair.innerHTML = flair.textContent.split(' ').map(function(segment) {
+				if (segment.match(/^https?:\/\//)) {
+					return '<a href="' + segment + '" target="_blank" rel="noopener noreferrer">' + segment + '</a>';
+				} else {
+					return segment;
+				}
+			}).join(' ');
+		}
 	});
 })();
