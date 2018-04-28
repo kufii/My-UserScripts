9ed9e3712ba2cbc4cf0eeebc240ec6e28f26c59b
Adrien Pyke <adpyke@gmail.com>
2018-04-28 19:33:02 -0400

    underline links on hover
    also fix for flairs for the random different dom with the article tag
    that only shows up in card mode

--- reddit-flair-linkifier.user.js
+++ reddit-flair-linkifier.user.js
@@ -1,16 +1,25 @@
 // ==UserScript==
 // @name         Reddit Flair Linkifier
 // @namespace    https://greasyfork.org/users/649
-// @version      2.0.1
+// @version      2.0.2
 // @description  Turns the text in various subreddits' flair into links
 // @author       Adrien Pyke
 // @match        *://*.reddit.com/*
 // @require      https://cdn.rawgit.com/fuzetsu/userscripts/477063e939b9658b64d2f91878da20a7f831d98b/wait-for-elements/wait-for-elements.js
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (() => {
 	'use strict';
+
+	GM_addStyle(`
+		.flair-link {
+			text-decoration: none;
+		}
+		.flair-link:hover {
+			text-decoration: underline;
+		}
+	`);
 
 	waitForElems({
 		sel: [
@@ -19,6 +28,7 @@
 
 			// card template
 			'.Post > div:nth-of-type(2) > div:nth-of-type(1) > div > div:nth-of-type(1) > div:nth-of-type(2) > span',
+			'.Post > div:nth-of-type(2) > article > div:nth-of-type(1) > div:nth-of-type(1) > div > div:nth-of-type(1) > div:nth-of-type(2) > span',
 			// classic template
 			'.Post > div:nth-of-type(2) > div > div:nth-of-type(2) > div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(2) > span',
 			// compact template
@@ -39,7 +49,7 @@
 		onmatch(flair) {
 			flair.innerHTML = flair.textContent.split(' ').map(segment => {
 				if (segment.match(/^https?:\/\//)) {
-					return `<a href="${segment}" target="_blank" rel="noopener noreferrer">${segment}</a>`;
+					return `<a href="${segment}" class="flair-link" target="_blank" rel="noopener noreferrer">${segment}</a>`;
 				} else {
 					return segment;
 				}
