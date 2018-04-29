69fee7c04356f0f2f688b41aaac0a63fb46239b2
Adrien Pyke <adpyke@gmail.com>
2018-04-28 20:18:58 -0400

    remove anti adblock overlay

--- rulu-ad-block.user.js
+++ rulu-ad-block.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Rulu.co remove ads
 // @namespace    https://greasyfork.org/users/649
-// @version      1.0
+// @version      1.0.1
 // @description  Removes ad links from Rulu.co
 // @author       Adrien Pyke
 // @match        *://www.rulu.co/*
+// @require      https://cdn.rawgit.com/fuzetsu/userscripts/477063e939b9658b64d2f91878da20a7f831d98b/wait-for-elements/wait-for-elements.js
 // @grant        none
 // ==/UserScript==
 
@@ -22,4 +23,11 @@
 	Util.qq('a')
 		.filter(link => link.href.match(REGEX))
 		.forEach(link => link.href = link.href.replace(REGEX, ''));
+
+	waitForElems({
+		sel: '#d0bf',
+		onmatch(overlay) {
+			overlay.remove();
+		}
+	});
 })();
