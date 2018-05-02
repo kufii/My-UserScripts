dab5ae16d4ca0e01ca79db5c1d3f4a9f45a8eb68
Adrien Pyke <adpyke@gmail.com>
2018-05-02 14:21:57 -0400

    fix url

--- reddit-prevent-middle-click.user.js
+++ reddit-prevent-middle-click.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         New reddit: Prevent middle click scroll
 // @namespace    https://greasyfork.org/users/649
-// @version      1.0
+// @version      1.0.1
 // @description  Prevents the middle click scroll when middle clicking posts on the new reddit layout
 // @author       Adrien Pyke
-// @match        url
+// @match        *://*.reddit.com/*
 // @grant        none
 // @require      https://cdn.rawgit.com/fuzetsu/userscripts/477063e939b9658b64d2f91878da20a7f831d98b/wait-for-elements/wait-for-elements.js
 // ==/UserScript==
