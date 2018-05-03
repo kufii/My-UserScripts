fc2dea77f26778d6cfe4a23bc953869c04a1c107
Adrien Pyke <adpyke@gmail.com>
2018-05-03 11:55:07 -0400

    run in iframe

--- userstyles-auto-keep-me-logged-in.user.js
+++ userstyles-auto-keep-me-logged-in.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         userstyles.org - auto select keep me logged in
 // @namespace    https://greasyfork.org/users/649
-// @version      1.0.2
+// @version      1.1
 // @description  Auto checks keep me logged in on userstyles.org
 // @author       Adrien Pyke
-// @match        *://userstyles.org/login*
+// @match        *://userstyles.org/d/login*
 // @grant        none
 // ==/UserScript==
 
