1fa63b68a5d8734d772f0230e8aeb621bd7084a9
Adrien Pyke <adpyke@gmail.com>
2018-05-07 21:45:15 -0400

    style updates

--- libs/gm_config.js
+++ libs/gm_config.js
@@ -49,36 +49,42 @@
 
 		const setup = function() {
 			const createContainer = function() {
-				let div = document.createElement('div');
-				div.style.backgroundColor = 'white';
-				div.style.padding = '5px';
-				div.style.border = '1px solid black';
-				div.style.position = 'fixed';
-				div.style.top = '0';
-				div.style.right = '0';
-				div.style.zIndex = 99999;
-				return div;
+				let form = document.createElement('form');
+				form.id = 'gm-config';
+				form.style.display = 'grid';
+				form.style.gridRowGap = '5px';
+				form.style.gridColumnGap = '10px';
+				form.style.backgroundColor = 'white';
+				form.style.padding = '5px';
+				form.style.border = '1px solid black';
+				form.style.position = 'fixed';
+				form.style.top = '0';
+				form.style.right = '0';
+				form.style.zIndex = 99999;
+				return form;
 			};
-			const createTextbox = function(value, placeholder) {
+			const createTextbox = function(name, value, placeholder) {
 				let input = document.createElement('input');
-				input.setAttribute('type', 'text');
+				input.type = 'text';
+				input.name = name;
 				input.value = value;
 				if (placeholder) {
 					input.setAttribute('placeholder', placeholder);
 				}
+				input.style.gridColumn = '2/4';
 				return input;
 			};
-			const createNumber = function(value, placeholder, min, max, step) {
-				let input = createTextbox(value, placeholder);
-				input.setAttribute('type', 'number');
+			const createNumber = function(name, value, placeholder, min, max, step) {
+				let input = createTextbox(name, value, placeholder);
+				input.type = 'number';
 				input.setAttribute('min', min);
 				input.setAttribute('max', max);
 				input.setAttribute('step', step);
 				return input;
 			};
-			const createSelect = function(lbl, options, value) {
+			const createSelect = function(name, lbl, options, value) {
 				let select = document.createElement('select');
-				select.style.margin = '2px';
+				select.name = name;
 				let optgroup = document.createElement('optgroup');
 				if (lbl) {
 					optgroup.setAttribute('label', lbl);
@@ -91,31 +97,34 @@
 					optgroup.appendChild(option);
 				});
 				select.value = value;
+				select.style.gridColumn = '2/4';
 				return select;
 			};
-			const createCheckbox = function(lbl, checked) {
-				let label = document.createElement('label');
+			const createCheckbox = function(name, lbl, checked) {
 				let checkbox = document.createElement('input');
-				checkbox.setAttribute('type', 'checkbox');
-				label.appendChild(checkbox);
-				label.querySelector('input').checked = checked;
-				label.appendChild(document.createTextNode(lbl));
-				return label;
+				checkbox.id = `gm-config-${name}`;
+				checkbox.type = 'checkbox';
+				checkbox.name = name;
+				checkbox.checked = checked;
+				checkbox.style.gridColumn = '2/4';
+				checkbox.style.alignSelf = 'center';
+				return checkbox;
 			};
-			const createButton = function(text, onclick) {
+			const createButton = function(text, onclick, gridColumn) {
 				let button = document.createElement('button');
 				button.style.margin = '2px';
 				button.textContent = text;
 				button.onclick = onclick;
+				button.style.gridColumn = gridColumn;
 				return button;
 			};
-			const createLabel = function(label) {
-				let lbl = document.createElement('span');
+			const createLabel = function(name, label) {
+				let lbl = document.createElement('label');
+				lbl.setAttribute('for', name);
 				lbl.textContent = label;
+				lbl.style.textAlign = 'right';
+				lbl.style.gridColumn = '1/2';
 				return lbl;
-			};
-			const createLineBreak = function() {
-				return document.createElement('br');
 			};
 			const init = function(cfg) {
 				let controls = {};
@@ -126,32 +135,26 @@
 
 					let control;
 					if (setting.type === 'text') {
-						control = createTextbox(value, setting.placeholder);
+						control = createTextbox(setting.key, value, setting.placeholder);
 					} else if (setting.type === 'number') {
-						control = createNumber(value, setting.placeholder, setting.min, setting.max, setting.step);
+						control = createNumber(setting.key, value, setting.placeholder, setting.min, setting.max, setting.step);
 					} else if (setting.type === 'dropdown') {
-						control = createSelect(setting.label, setting.values, value);
+						control = createSelect(setting.key, setting.label, setting.values, value);
 					} else if (setting.type === 'bool') {
-						control = createCheckbox(setting.label, value);
+						control = createCheckbox(setting.key, setting.label, value);
 					}
 
-					if (setting.type === 'bool') {
-						controls[setting.key] = control.querySelector('input');
-					} else {
-						controls[setting.key] = control;
-						div.appendChild(createLabel(`${setting.label}: `));
-					}
+					div.appendChild(createLabel((setting.type === 'bool' ? 'gm-config-' : '') + setting.key, setting.label));
+					div.appendChild(control);
+					controls[setting.key] = control;
 
-					controls[setting.key].addEventListener(events[setting.type], () => {
+					control.addEventListener(events[setting.type], () => {
 						if (ret.onchange) {
 							let control = controls[setting.key];
 							let value = setting.type === 'bool' ? control.checked : control.value;
 							ret.onchange(setting.key, value);
 						}
 					});
-
-					div.appendChild(control);
-					div.appendChild(createLineBreak());
 				});
 
 				div.appendChild(createButton('Save', () => {
@@ -167,14 +170,14 @@
 					}
 
 					div.remove();
-				}));
+				}, '2/3'));
 
 				div.appendChild(createButton('Cancel', () => {
 					if (ret.oncancel) {
 						ret.oncancel(cfg);
 					}
 					div.remove();
-				}));
+				}, '3/4'));
 
 				document.body.appendChild(div);
 			};
