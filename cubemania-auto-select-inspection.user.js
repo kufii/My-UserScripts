dd30104af94f103b22c621fc7db5af64b1907042
Adrien Pyke <adpyke@gmail.com>
2018-05-26 21:35:29 -0400

    Allow to set per puzzle

--- cubemania-auto-select-inspection.user.js
+++ cubemania-auto-select-inspection.user.js
@@ -1,15 +1,37 @@
 // ==UserScript==
 // @name         Cubemania - Auto Select Inspection
 // @namespace    https://greasyfork.org/users/649
-// @version      1.0.1
+// @version      1.1
 // @description  auto checkes the 15 second inspection checkbox on the cubemania timer
 // @author       Adrien Pyke
 // @match        *://www.cubemania.org/puzzles/*/timer
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 (() => {
 	'use strict';
 
-	document.querySelector('input.inspection-toggle').click();
+	const Config = {
+		getAutoSelect(key) {
+			const value = GM_getValue(key);
+			if (typeof value === 'undefined') return true;
+			return value;
+		},
+		setAutoSelect(key, value) {
+			GM_setValue(key, value);
+		}
+	};
+
+	const puzzle = location.pathname.match(/^\/puzzles\/(.+)\//i)[1];
+	const autoselect = Config.getAutoSelect(puzzle);
+	if (autoselect) {
+		document.querySelector('input.inspection-toggle').click();
+	}
+
+	GM_registerMenuCommand(`${autoselect ? 'Disable' : 'Enable'} "Auto Select Inspection" for ${puzzle}`, () => {
+		Config.setAutoSelect(puzzle, !autoselect);
+		location.reload();
+	});
 })();
