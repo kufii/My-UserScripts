6dc2e72c25f7f9e7156808c6d6219bd5e8b6a906
Adrien Pyke <adpyke@gmail.com>
2018-09-06 16:13:32 -0400

    use new JS apis and support new search urls

--- greasyfork-default-sort.user.js
+++ greasyfork-default-sort.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Greasy Fork - Change Default Script Sort
 // @namespace    https://greasyfork.org/users/649
-// @version      1.2.19
+// @version      1.3
 // @description  Change default script sort on GreasyFork
 // @author       Adrien Pyke
 // @match        *://greasyfork.org/*/users/*
@@ -15,40 +15,6 @@
 
 (() => {
 	'use strict';
-
-	const Util = {
-		q(query, context = document) {
-			return context.querySelector(query);
-		},
-		getQueryParam(name, url = location.href) {
-			name = name.replace(/[[\]]/g, '\\$&');
-			const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`);
-			const results = regex.exec(url);
-			if (!results) return null;
-			if (!results[2]) return '';
-			return decodeURIComponent(results[2].replace(/\+/g, ' '));
-		},
-		setQueryParam(key, value, url = location.href) {
-			const regex = new RegExp(`([?&])${key}=.*?(&|#|$)(.*)`, 'gi');
-			const hasValue = (typeof value !== 'undefined' && value !== null && value !== '');
-			if (regex.test(url)) {
-				if (hasValue) {
-					return url.replace(regex, `$1${key}=${value}$2$3`);
-				} else {
-					const [path, hash] = url.split('#');
-					url = path.replace(regex, '$1$3').replace(/(&|\?)$/, '');
-					if (hash) url += `#${hash[1]}`;
-					return url;
-				}
-			} else if (hasValue) {
-				const separator = url.includes('?') ? '&' : '?';
-				const [path, hash] = url.split('#');
-				url = `${path + separator + key}=${value}`;
-				if (hash) url += `#${hash[1]}`;
-				return url;
-			} else return url;
-		}
-	};
 
 	const commonValues = [
 		{ value: 'daily-installs', text: 'Daily installs' },
@@ -81,34 +47,34 @@
 	]);
 	GM_registerMenuCommand('GreasyFork Sort Settings', Config.setup);
 
-	const onScripts = location.href.match(/^https?:\/\/greasyfork\.org\/[^/]+\/scripts\/?(?:\?.*)?$/i);
-	const onSearch = location.href.match(/^https?:\/\/greasyfork\.org\/[^/]+\/scripts\/search?(?:\?.*)?$/i);
-	const onProfile = location.href.match(/^https?:\/\/greasyfork\.org\/[^/]+\/users\/[^/]+?(?:\?.*)?$/i);
+	const onSearch = location.href.match(/^https?:\/\/greasyfork\.org\/.+?\/scripts\/?.*\?.*q=/i);
+	const onScripts = location.href.match(/^https?:\/\/greasyfork\.org\/.+?\/scripts\/?/i);
+	const onProfile = location.href.match(/^https?:\/\/greasyfork\.org\/.+?\/users\//i);
 
 	document.addEventListener('DOMContentLoaded', () => {
-		const defaultSort = Util.q('#script-list-sort > ul > li:nth-child(1) > a');
+		const defaultSort = document.querySelector('#script-list-sort > ul > li:nth-child(1) > a');
 		if (defaultSort) {
-			if (onSearch) {
-				defaultSort.href = Util.setQueryParam('sort', 'relevance', defaultSort.href);
-			} else {
-				defaultSort.href = Util.setQueryParam('sort', 'daily-installs', defaultSort.href);
-			}
+			const url = new URL(defaultSort.href);
+			url.searchParams.set('sort', onSearch ? 'relevance' : 'daily-installs');
+			defaultSort.href = url.href;
 		}
 	});
 
-	const sort = Util.getQueryParam('sort');
+	const url = new URL(location.href);
+	const sort = url.searchParams.get('sort');
 	if (!sort) {
 		const cfg = Config.load();
 		let cfgSort;
-		if (onScripts) {
-			cfgSort = cfg.all;
-		} else if (onSearch) {
+		if (onSearch) {
 			cfgSort = cfg.search;
+		} else if (onScripts) {
+			cfgSort = cfg.all;
 		} else if (onProfile) {
 			cfgSort = cfg.user;
 		}
 		if (cfgSort) {
-			window.location.replace(Util.setQueryParam('sort', cfgSort));
+			url.searchParams.set('sort', cfgSort);
+			window.location.replace(url.href);
 		}
 	}
 })();
