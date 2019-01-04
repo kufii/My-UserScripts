ad3dec2fdba4f1a247b259971885599d7c44d439
Adrien Pyke <adpyke@gmail.com>
2019-01-04 13:30:48 -0500

    only convert text

--- reddit-flair-linkifier.user.js
+++ reddit-flair-linkifier.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit Flair Linkifier
 // @namespace    https://greasyfork.org/users/649
-// @version      2.1.1
+// @version      2.1.2
 // @description  Turns the text in various subreddits' flair into links
 // @author       Adrien Pyke
 // @match        *://*.reddit.com/*
@@ -33,7 +33,7 @@
 			`${newLayoutId} span`
 		].join(','),
 		onmatch(flair) {
-			if (flair.childNodes.length !== 1) return;
+			if (flair.childNodes.length !== 1 || flair.childNodes[0].nodeType !== Node.TEXT_NODE) return;
 			flair.innerHTML = flair.textContent.split(' ').map(segment => {
 				if (segment.match(/^https?:\/\//)) {
 					return `<a href="${segment}" class="flair-link" target="_blank" rel="noopener noreferrer">${segment}</a>`;
