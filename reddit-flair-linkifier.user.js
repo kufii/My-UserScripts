54f1368b86477a72535e593b57f20911f9215299
Adrien Pyke <adpyke@gmail.com>
2019-01-07 11:12:12 -0500

    don't convert within editor

--- reddit-flair-linkifier.user.js
+++ reddit-flair-linkifier.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit Flair Linkifier
 // @namespace    https://greasyfork.org/users/649
-// @version      2.1.3
+// @version      2.1.4
 // @description  Turns the text in various subreddits' flair into links
 // @author       Adrien Pyke
 // @match        *://*.reddit.com/*
@@ -33,7 +33,8 @@
 			`${newLayoutId} span`
 		].join(','),
 		onmatch(flair) {
-			if (flair.childNodes.length !== 1 || flair.childNodes[0].nodeType !== Node.TEXT_NODE) return;
+			if (flair.childNodes.length !== 1 || flair.childNodes[0].nodeType !== Node.TEXT_NODE || flair.closest('.DraftEditor-root')) return;
+
 			const newhtml = flair.textContent.split(' ').map(segment => {
 				if (segment.match(/^https?:\/\//)) {
 					return `<a href="${segment}" class="flair-link" target="_blank" rel="noopener noreferrer">${segment}</a>`;
