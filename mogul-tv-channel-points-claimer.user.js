a1c40e1bfe2fd91ada56c94ca728393169b1f8db
Adrien Pyke <adpyke@gmail.com>
2022-01-30 17:00:00 -0500

    mogul tv userscript

--- mogul-tv-channel-points-claimer.user.js
+++ mogul-tv-channel-points-claimer.user.js
@@ -1,14 +1,20 @@
 // ==UserScript==
-// @name         name
+// @name         Mogul TV Channel Points Claimer
 // @namespace    https://greasyfork.org/users/649
 // @version      1.0
-// @description  description
+// @description  Auto claim point on ludwig streams
 // @author       Adrien Pyke
-// @match        *://www.youtube.com/watch?*
+// @match        *://ludwig.social/*
 // @grant        none
 // @require      https://gitcdn.link/repo/fuzetsu/userscripts/b38eabf72c20fa3cf7da84ecd2cefe0d4a2116be/wait-for-elements/wait-for-elements.js
 // ==/UserScript==
 
 (() => {
   'use strict';
+
+  waitForElems({
+    sel: '.z-channel-points .claim.is-visible',
+    config: { attributes: true, childList: true, subtree: true },
+    onmatch: node => node.click()
+  });
 })();
