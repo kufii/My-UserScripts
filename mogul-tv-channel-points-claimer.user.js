26b26987d6b98577b1b857f1628ccbf8c0da654e
Adrien Pyke <adpyke@gmail.com>
2022-04-03 22:48:48 -0400

    fix Mogul TV channel points claimer

--- mogul-tv-channel-points-claimer.user.js
+++ mogul-tv-channel-points-claimer.user.js
@@ -1,20 +1,18 @@
 // ==UserScript==
 // @name         Mogul TV Channel Points Claimer
 // @namespace    https://greasyfork.org/users/649
-// @version      1.0
-// @description  Auto claim point on ludwig streams
+// @version      1.1
+// @description  Auto claim points on ludwig streams
 // @author       Adrien Pyke
 // @match        *://ludwig.social/*
 // @grant        none
-// @require      https://gitcdn.link/repo/fuzetsu/userscripts/b38eabf72c20fa3cf7da84ecd2cefe0d4a2116be/wait-for-elements/wait-for-elements.js
 // ==/UserScript==
 
 (() => {
   'use strict';
 
-  waitForElems({
-    sel: '.z-channel-points .claim.is-visible',
-    config: { attributes: true, childList: true, subtree: true },
-    onmatch: node => node.click()
-  });
+  setInterval(() => {
+    const node = document.querySelector('.z-channel-points .claim.is-visible');
+    if (node) node.click();
+  }, 1000);
 })();
