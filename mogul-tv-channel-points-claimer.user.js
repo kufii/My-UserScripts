f414850c6d3fae58b86a9e5ffaf890893cf32f5a
Adrien Pyke <adpyke@gmail.com>
2022-09-27 21:11:44 -0400

    support fuslie streams

--- mogul-tv-channel-points-claimer.user.js
+++ mogul-tv-channel-points-claimer.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Truffle TV Channel Points Claimer
 // @namespace    https://greasyfork.org/users/649
-// @version      2.0
-// @description  Auto claim point on ludwig streams
+// @version      2.1
+// @description  Auto claim point on Truffle TV enabled streams. Currently works for Ludwig and Fuslie.
 // @author       Adrien Pyke
 // @match        *://new.ludwig.social/channel-points
+// @match        *://fuslie.spore.build/component-instance/*
 // @grant        none
 // ==/UserScript==
 
@@ -12,8 +13,11 @@
   'use strict';
 
   setInterval(() => {
-    const root = document.getElementById('root').firstChild.shadowRoot.firstChild.shadowRoot;
-    const node = root.querySelector('.claim');
+    const root = document.getElementById('root');
+    const shadowRoot = root && root.firstChild.shadowRoot.firstChild.shadowRoot;
+    const node = shadowRoot
+      ? shadowRoot.querySelector('.claim')
+      : document.querySelector('.claim.is-visible');
     if (node) node.click();
   }, 1000);
 })();
